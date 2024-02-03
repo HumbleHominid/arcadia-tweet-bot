a7ab793465b4a3201377a978350e1f88e064a610
HumbleHominid <michael.d.fryer@gmail.com>
2024-02-03 12:01:51 -0700

    Working V1 of the video posting bot. Does not continually poll every 10m

--- bot.py
+++ bot.py
@@ -1,73 +1,116 @@
+# ---------------------
+# Arcadia Twitter Bot
+#
+# Author: HumbleHominid
+# ---------------------
 import os
 import json
-from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
+import googleapiclient.discovery
+import google_auth_oauthlib.flow
+import google.oauth2
+import google.auth.transport.requests
 import tweepy
+
+# Twitter API credentials
+from twitter_creds import *
 
 # YouTube API setup
 SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
-CHANNEL_IDS = ["CHANNEL_ID_1", "CHANNEL_ID_2"]  # Replace with the actual channel IDs
+SECRETS_FILE = "secrets.json"
+TOKEN_FILE = "token.json"
 
-# Twitter API setup
-TWITTER_API_KEY = "YOUR_TWITTER_API_KEY"
-TWITTER_API_SECRET_KEY = "YOUR_TWITTER_API_SECRET_KEY"
-TWITTER_ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN"
-TWITTER_ACCESS_TOKEN_SECRET = "YOUR_TWITTER_ACCESS_TOKEN_SECRET"
+# List of members YouTube channels and twitter IDs
+ARCADIA_MEMBERS = [
+    # [YouTube Channel ID, Twitter Handle]
+]
 
+# Cached latest videos
+LATEST_VIDEOS_FILE = "latest_videos.json"
+
+# Authenticates YouTube and returns an api object
 def authenticate_youtube():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json')
+    if os.path.exists(TOKEN_FILE):
+        creds = google.oauth2.credentials.Credentials.from_authorized_user_file(TOKEN_FILE)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            creds.refresh(google.auth.transport.requests.Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(SECRETS_FILE, SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    
-    return build(API_SERVICE_NAME, API_VERSION, credentials=creds)
 
-def authenticate_twitter():
-    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
-    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    return api
+        with open(TOKEN_FILE, 'w') as token_file:
+            token_file.write(creds.to_json())
 
-def get_latest_video_id(api, channel_id):
-    response = api.search(q=f"from:{channel_id} filter:videos", count=1, result_type="recent")
-    if response:
-        return response[0].id_str
-    return None
+    return googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=creds)
 
-def post_tweet(api, channel_id, video_id):
-    tweet_text = f"New video alert! Check out the latest video from {channel_id} - https://www.youtube.com/watch?v={video_id}"
-    api.update_status(status=tweet_text)
+# Loads the latest videos members have posted from the saved video file into memory
+def load_latest_videos():
+    latest_vids = {}
+
+    if os.path.exists(LATEST_VIDEOS_FILE):
+        with (open(LATEST_VIDEOS_FILE, 'r') as videos_file):
+            try:
+                latest_vids = json.load(videos_file)
+            except: None
+
+    return latest_vids
+
+# Creates an Oauth1 twitter client
+def create_twitter_client():
+    client = tweepy.Client(
+        consumer_key=TWITTER_API_KEY,
+        consumer_secret=TWITTER_API_SECRET_KEY,
+        access_token=TWITTER_ACCESS_TOKEN,
+        access_token_secret=TWITTER_ACCESS_TOKEN_SECRET
+    )
+    return client
+
+# Posts a tweet given a video title, links to the video, and tags the member on twitter
+def post_tweet(client, video_title, video_id, member_twitter_handle=None):
+    tweet_text = f"{video_title} https://www.youtube.com/watch?v={video_id}"
+
+    ## Only tag the member if the twitter handle is not empty
+    if member_twitter_handle:
+        tweet_text += f" @{member_twitter_handle}"
+
+    try:
+        client.create_tweet(text=tweet_text)
+    except: None
 
 def main():
     youtube_api = authenticate_youtube()
-    twitter_api = authenticate_twitter()
+    twitter_client = create_twitter_client()
+    latest_vids = load_latest_videos()
 
-    for channel_id in CHANNEL_IDS:
-        latest_video_id = get_latest_video_id(twitter_api, channel_id)
+    for arcadia_member in ARCADIA_MEMBERS:
+        channel_id = arcadia_member[0]
         request = youtube_api.search().list(
-            part="snippet",
+            part ="snippet",
             channelId=channel_id,
             order="date",
             type="video",
             maxResults=1
         )
         response = request.execute()
-        new_video_id = response['items'][0]['id']['videoId']
 
-        if new_video_id != latest_video_id:
-            post_tweet(twitter_api, channel_id, new_video_id)
+        if response['items']:
+            video_id = response['items'][0]['id']['videoId']
+
+            # Only post tweet if it's a new video we don't know about
+            if not channel_id in latest_vids or latest_vids[channel_id] != video_id:
+                video_title = response['items'][0]['snippet']['title']
+                latest_vids[channel_id] = video_id
+
+                # Send tweet
+                post_tweet(twitter_client, video_title, video_id, arcadia_member[1])
+
+    # Write the latest videos to a file for reading in later
+    with (open(LATEST_VIDEOS_FILE, 'w') as videos_file):
+        json.dump(latest_vids, videos_file)
 
 if __name__ == "__main__":
     main()
